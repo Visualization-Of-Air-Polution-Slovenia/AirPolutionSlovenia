c9ef6441ad1ba496c924717eaf1619a2fb633e12
LukaRiz <92185057+LukaRiz@users.noreply.github.com>
2025-12-18 18:59:54 +0100

    added more data pm10 arso

--- airbase_historical_extractor.py
+++
@@ -1,9 +0,0 @@
-import airbase
-
-client = airbase.AirbaseClient()
-
-country = 'SI'
-pollutants = ['O3', 'C6H6', 'CO', 'NO2', 'NOx', 'PM10', 'PM2.5', 'SO2']
-
-request = airbase.AirbaseRequest(airbase.Dataset.Historical, country, poll=pollutants)
-request.download("./data/EEA_historical_data/raw")
