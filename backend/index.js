919e52ee51063b6694a34e43fca1c42da55c627c
jimocool2 <muzan.jure@gmail.com>
2025-12-31 10:47:38 +0100

    Fix backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
 
 // Render provides PORT in environment
 const PORT = process.env.PORT || 3000;
